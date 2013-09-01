1f8b1aa752922d71d472ce8f70b5724f8ee731fc
Caidie <Admin@student-109-wless94-216.rice.edu>
2013-08-31 21:21:39 -0500

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Caidie on 8/31/13.
+//  Copyright (c) 2013 Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
