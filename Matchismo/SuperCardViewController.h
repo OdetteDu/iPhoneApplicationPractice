b41dc601d47f598aa204eaf154751baa8d5a9148
OdetteDu <Odette.Du@rice.edu>
2013-09-22 19:53:30 -0500

    Demo from Lecture 6

---
+++ Matchismo/SuperCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuperCardViewController.h
+//  Matchismo
+//
+//  Created by Caidie on 9/22/13.
+//  Copyright (c) 2013 Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperCardViewController : UIViewController
+
+@end
