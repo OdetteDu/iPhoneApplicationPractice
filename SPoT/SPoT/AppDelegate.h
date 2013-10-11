8ee0c2354acba6d9310e6ec21b5d68acf6a0904a
OdetteDu <Odette.Du@rice.edu>
2013-10-10 21:40:57 -0500

    setup

---
+++ SPoT/SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Caidie on 10/10/13.
+//  Copyright (c) 2013 Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
